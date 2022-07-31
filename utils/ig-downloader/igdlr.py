937fdef9c4116dbc27135d9c24319909515c4901
zwolf13 <zwolf13@gmail.com>
2022-07-31 09:46:25 -0400

    Updated igdlr and yt-monitor

--- utils/ig-downloader/igdlr.py
+++ utils/ig-downloader/igdlr.py
@@ -6,67 +6,58 @@ import sys
 from datetime import datetime
 from http.cookiejar import MozillaCookieJar
 
-OUTPUT_FOLDER = '/home/pi/tmp/ig-downloader/downloads'
+OUTPUT_FOLDER = 'C:/Users/Mauricio/dwhelper/Instagram'
+DEBUG = False
 
 def get_cookie_header():
-    csrftoken = 'Z5opmD9LaNKbZiHr0OGHYFpgkE7gfj6W'
-    mid = 'YjxsUgAEAAH3n1g3uBCKQwAF5hCv'
-    ig_did = 'ACB08B6F-D252-4FC7-81ED-4EECFE23CBB6'
-    built = f'csrftoken={csrftoken}; mid={mid}; ig_did={ig_did}; ig_nrcb=1; fbm_124024574287414=base_domain=.instagram.com; ds_user_id=3452129080; sessionid=3452129080%3AHISxmbMWZysBkB%3A25; shbid="19869\0543452129080\0541684933480:01f7da7dc0bcdb4ef933fb3a1f24f2f3766053bde9cfb0c75996e4cae6b9a043615aa460"; shbts="1653397480\0543452129080\0541684933480:01f7ec716c837804dade3e17f2831651e9c7fe837d855bb4c33752c93a962ae14c07bdb1"; fbsr_124024574287414=hBFgbp8_-csv9ZMPEgO9bTth-Jj8pcrApcYDjHMSPgs.eyJ1c2VyX2lkIjoiNjAyNjE5Nzg2IiwiY29kZSI6IkFRQmRUaEFjbDFBWGNmN3ktQ1BhdHRzbGs3ZE1ZMWo0YUROekdtSHpJVUZRdkE1Nk1tc2pBSHB6d2htSzd2MDFEdlZZbF9tZUhwUTBfcVNyQXgyWEVIOFhHUVFNQ3g4eDlrb1JHdjVzdE8xOVozMHpWNUtQWWNDRGJuQUlfbngyX0hBczBacFRCWlJrQzNDdzFmZk9ad0xtZ3NwcS0wMHU5eHlWelBGaGZERmVBVTAtV2U3U2MyM0J4R3FuZHE0ekpMV043Zm13RGp3dk5iYTVqbm5VTXV0a3VXZnpfR04wNEJPbURfdWgyTzFzSHRSZnJwOHFGbXFvSTlPb1R0Vm5UVkRQQXRzZndKUXloWFJpZXEwcXU1SmNuX1NNZ0Z4dUhMMzhxQndKemVRMmNZd1JJN2NRcFRwdnJOdHVUTldZOGkwIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQUFTbjZVMHFoQnBpaVpBeHhrZnA5MWVEbGI3RHhsNjFqQU5lcGJIU0psTEllWkNHaEVxNk1yckk4Vmd6U3JDVW52SzFRbXZtNkVHYmk3VXpZbXhnTlpDeHV3ZEZMT2p2SjdhbUtCSFpDd292cFhtalk1dVVtbVN3WTQ3OHdSZTJaQko1c3JlSzRsTTh1SkRZMXF5MVpCMTVPbFJCWkJUUHp0aFhrTHpWdzNlIiwiYWxnb3JpdGhtIjoiSE1BQy1TSEEyNTYiLCJpc3N1ZWRfYXQiOjE2NTM1OTI3ODV9; rur="NCG\0543452129080\0541685128538:01f79d1a9ee1aa131793acd9c7d2255efe098c75ca7d7ca5ce3f76774b51d2acbfe3904e"'
+    return 'csrftoken=aMSU26xkWCSMqYBqTKfmXU7oypTAX9Od; mid=YtVnLAAEAAGwNT-WIsVuz-ohGSs5; ig_did=C3D3162C-1019-4012-87A4-1DB6201F23B1; ig_nrcb=1; ds_user_id=3452129080; sessionid=3452129080%3AQviA2eUVAUMZVb%3A6%3AAYeSasN7kXGs7vH5bagdOH0meIA8fmxtd2QDi6BfpQ; shbid="19869\0543452129080\0541690297172:01f73a9c6de88e8e3657b9138a3a7e6514cc06a7c5d66d3279e9ba4d29c9826eebdc2c8e"; shbts="1658761172\0543452129080\0541690297172:01f779128f5c59d2a8690d0cc5679e087b95f0f7ceca16478203a9db176c5ede3930ee71"; datr=eWfVYnecei_NfGDgohf3_lJV; fbm_124024574287414=base_domain=.instagram.com; rur="NCG\0543452129080\0541690298835:01f712db339d4ee4ea98a7a1b9bdaf48913d22cc614258d59072f19699b7f918bbd16132"; fbsr_124024574287414=xrSBpBpkXf_fJx87vWZ4QgnocY-cwI1aADccfIEXSDQ.eyJ1c2VyX2lkIjoiNjAyNjE5Nzg2IiwiY29kZSI6IkFRQnF2dllXU2QtY1NQRWV6d3BZOXpyNlJXY21uRl9FOWQ1TXBjWlZvX1l3UkFTQnB0T3FqUlhFdEE4VXVjTnVtYWFoT2JpTTM4TF9kTnB2QzFLVG5ubE56S1RtSWxfTGRveFBYVFN5dDVCd05wdE4xVUNFMmtBeS1BZmxfQ3FNUjZ6OG9wSFlWN093MzR5OXJ3YjQ3VnUtNXlaRjdLQnlYZFItbDgxVVFueTBFaHZCb1I0QXJVLS1VOG9BU014Q1FLTGpvRHpjM21jTnd3Tjc5SS0xUHBGMGtrX1FqMzFIaEpqemwyQTN5a1NhWThadWpDR0VrdHYwLVozeDRncUFEZWZhTVNiZzNvTDZkV1o5TkwySE5naVMxRGFrU3lKSUplLWtqUS1rajkzSVdTRklSYlpOek9lLUZuX2llMWdIc0dJIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQU1KNXBvUWxnUmF2OXFmaHU4Z29Fd2Z0MnV1bElIN0hUeGhvRVRUZHRIdnVRNzdNSFJ6WDU3MWc2TUYxdHRQQjcyaEtJcHdodkFzSjNtRzc5S0toNTJiYkNpbmlwN3dJbVVYUzNrMkFEMFZPZ3R5WkE0anZoS0NlR0ZaQk13UVVFb2NuSXRUY0JSR244dFpCajVSS21CQndYOXBkTDYwc0RyTWxlMVYiLCJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImlzc3VlZF9hdCI6MTY1ODc2MjgxOH0'
 
-    return 'csrftoken=Z5opmD9LaNKbZiHr0OGHYFpgkE7gfj6W; mid=YjxsUgAEAAH3n1g3uBCKQwAF5hCv; ig_did=ACB08B6F-D252-4FC7-81ED-4EECFE23CBB6; ig_nrcb=1; fbm_124024574287414=base_domain=.instagram.com; ds_user_id=3452129080; sessionid=3452129080%3AHISxmbMWZysBkB%3A25; shbid="19869\0543452129080\0541684933480:01f7da7dc0bcdb4ef933fb3a1f24f2f3766053bde9cfb0c75996e4cae6b9a043615aa460"; shbts="1653397480\0543452129080\0541684933480:01f7ec716c837804dade3e17f2831651e9c7fe837d855bb4c33752c93a962ae14c07bdb1"; fbsr_124024574287414=hBFgbp8_-csv9ZMPEgO9bTth-Jj8pcrApcYDjHMSPgs.eyJ1c2VyX2lkIjoiNjAyNjE5Nzg2IiwiY29kZSI6IkFRQmRUaEFjbDFBWGNmN3ktQ1BhdHRzbGs3ZE1ZMWo0YUROekdtSHpJVUZRdkE1Nk1tc2pBSHB6d2htSzd2MDFEdlZZbF9tZUhwUTBfcVNyQXgyWEVIOFhHUVFNQ3g4eDlrb1JHdjVzdE8xOVozMHpWNUtQWWNDRGJuQUlfbngyX0hBczBacFRCWlJrQzNDdzFmZk9ad0xtZ3NwcS0wMHU5eHlWelBGaGZERmVBVTAtV2U3U2MyM0J4R3FuZHE0ekpMV043Zm13RGp3dk5iYTVqbm5VTXV0a3VXZnpfR04wNEJPbURfdWgyTzFzSHRSZnJwOHFGbXFvSTlPb1R0Vm5UVkRQQXRzZndKUXloWFJpZXEwcXU1SmNuX1NNZ0Z4dUhMMzhxQndKemVRMmNZd1JJN2NRcFRwdnJOdHVUTldZOGkwIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQUFTbjZVMHFoQnBpaVpBeHhrZnA5MWVEbGI3RHhsNjFqQU5lcGJIU0psTEllWkNHaEVxNk1yckk4Vmd6U3JDVW52SzFRbXZtNkVHYmk3VXpZbXhnTlpDeHV3ZEZMT2p2SjdhbUtCSFpDd292cFhtalk1dVVtbVN3WTQ3OHdSZTJaQko1c3JlSzRsTTh1SkRZMXF5MVpCMTVPbFJCWkJUUHp0aFhrTHpWdzNlIiwiYWxnb3JpdGhtIjoiSE1BQy1TSEEyNTYiLCJpc3N1ZWRfYXQiOjE2NTM1OTI3ODV9; rur="NCG\0543452129080\0541685128538:01f79d1a9ee1aa131793acd9c7d2255efe098c75ca7d7ca5ce3f76774b51d2acbfe3904e"'
-
-def download_data(igid):
-    url = f'https://www.instagram.com/p/{igid}/'
-    print(f'Downloading data from: {url}...')
-
+def download_html(url):
+    print(f'Downloading HTML from: {url}...')
+    
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0',
-        'Cookie': get_cookie_header()
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
+        'Cookie': get_cookie_header(),
+        'X-IG-App-ID': '936619743392459',
     }
     html_page = requests.get(url, headers = headers)
-
+    
     if html_page.status_code == 200:
         content = html_page.text
-#        write_file(content, 'instagram.html')
-        page_title = re.search(r"\<title\>$\n(.*?)$\n\<\/title\>", content, re.M).group(1)
-
-        if 'login' in page_title.lower():
-            print("Couldn't login :'(")
-            return {}
-        else:
-            print('Parsing additional data loaded...')
-            additionalDataLoaded = re.search(f"window\.__additionalDataLoaded\(\'/p/{igid}/\',(.*?)\);<\/script>", content).group(1)
-            additional_data = json.loads(additionalDataLoaded)
-            return additional_data
+        if DEBUG:
+            write_file(content, 'instagram.html')
+        return content
     else:
         print("Couldn't download HTML page :'(")
-        return {}
+        return None
 
 def get_item_metadata(item):
     print('Getting iteam metadata...')
+    caption = item['caption']
+    
     metadata = {}
     metadata['id'] = item['code']
     metadata['uploader_id'] = item['user']['username']
     metadata['title'] = f"Post by {metadata['uploader_id']}"
     metadata['fulltitle'] = metadata['title']
-    metadata['description'] = item['caption']['text']
-    metadata['timestamp'] = item['caption']['created_at']
+    metadata['description'] = caption['text'] if caption is not None else None
+    metadata['timestamp'] = caption['created_at'] if caption is not None else None
     metadata['uploader'] = item['user']['full_name']
     metadata['like_count'] = item['like_count']
-    metadata['comment_count'] = item['comment_count']
+    metadata['comment_count'] = item.get('comment_count')
     metadata['extractor'] = 'Instagram'
     metadata['webpage_url'] = f"https://www.instagram.com/p/{metadata['id']}"
     metadata['webpage_url_basename'] = metadata['id']
     metadata['extractor_key'] = 'Instagram'
     metadata['display_id'] = metadata['id']
-    metadata['upload_date'] = datetime.fromtimestamp(metadata['timestamp']).strftime("%Y%m%d")
+    metadata['upload_date'] = datetime.fromtimestamp(metadata['timestamp']).strftime("%Y%m%d") if metadata['timestamp'] is not None else None
+    
     return metadata
 
 def download_media(url, filename):
     print('Downloading media...')
     image = requests.get(url, stream = True)
-
+    
     if image.status_code == 200:
         image.raw.decode_content = True
         with open(f'{OUTPUT_FOLDER}/{filename}', 'wb') as f:
@@ -75,10 +66,23 @@ def download_media(url, filename):
     else:
         print("Couldn't download image :'(")
 
+def download_media_info(media_id):
+    url = f'https://i.instagram.com/api/v1/media/{media_id}/info/'
+    print(f'Downloading media info from: {url}...')
+    headers = {
+        'Cookie': get_cookie_header(),
+        'X-IG-App-ID': '936619743392459'
+    }
+    media_info = requests.get(url, headers = headers)
+    media_info_json = json.loads(media_info.text)
+    if DEBUG:
+        write_file(media_info_json, 'media_info.json', is_json = True)
+    return media_info_json
+
 def write_file(object, filename, is_json = False):
     print(f'Saving file: {filename}...')
     output_text = json.dumps(object, indent = 4) if is_json else object
-    with open(f'{OUTPUT_FOLDER}/{filename}', 'w') as f:
+    with open(f'{OUTPUT_FOLDER}/{filename}', 'w', encoding='utf-8') as f:
         f.write(output_text)
 
 def get_image_url_from_versions(image_versions2, metadata):
@@ -151,17 +155,48 @@ def process_video(item, carousel_media = None, index = None):
     download_media(url, image_filename)
     write_file(metadata, f"{base_filename}.info.json", is_json = True)
 
-if len(sys.argv) < 2:
-   print('Error, no IG ID found!')
-   exit(0)
+### Command Line input
 
-igid = sys.argv[1]
-data_loaded = download_data(igid)
-# write_file(data_loaded, 'data_loaded.json', is_json = True)
+#if len(sys.argv) < 2:
+#   print('Error, no IG ID found!')
+#   exit(0)
 
-for item in data_loaded.get('items', []):
-    print('Processing item from data loaded...')
-    metadata = get_item_metadata(item)
-    process_image(item)
-    process_video(item)
-    process_carousel(item)
+#igid = sys.argv[1]
+
+### urls.txt file input
+
+# urls = []
+# with open('urls.txt') as f:
+#     urls = f.read().splitlines()
+
+# for url in urls:
+#     html_data = download_html(url)
+#     media_id = re.search(r'"media_id":"([0-9]*)"', html_data, re.M).group(1)
+#     media_info = download_media_info(media_id)
+    
+#     for item in media_info.get('items', []):
+#         print('Processing item from data loaded...')
+#         metadata = get_item_metadata(item)
+#         process_image(item)
+#         process_video(item)
+#         process_carousel(item)
+#         with open("success.txt", "a") as myfile:
+#             myfile.write(f'{url}\n')
+
+### media_ids.txt file input
+
+media_ids = []
+with open('media_ids.txt') as f:
+    media_ids = f.read().splitlines()
+
+for media_id in media_ids:
+    media_info = download_media_info(media_id)
+    
+    for item in media_info.get('items', []):
+        print('Processing item from data loaded...')
+        metadata = get_item_metadata(item)
+        process_image(item)
+        process_video(item)
+        process_carousel(item)
+        with open("success.txt", "a") as myfile:
+            myfile.write(f'{media_id}\n')
